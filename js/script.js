2fe419deef49f85228c57688a88a72ad46a325da
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-29 12:40:16 +0200

    Aggiunta lista delle chat con v-for

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // Inserisci il codice qui
+var app = new Vue(
+  {
+    el: '#chat',
+    data: {
+      listaImmagini: ['img/avatar_1.jpg',
+                      'img/avatar_2.jpg',
+                      'img/avatar_3.jpg',
+                      'img/avatar_4.jpg',
+                      'img/avatar_5.jpg',
+                      'img/avatar_6.jpg',
+                      'img/avatar_7.jpg',
+                      'img/avatar_8.jpg'
+                      ],
+      listaChat: ['Michele', 'Fabio', 'Samuele', 'Luisa', 'Samantha', 'Sofia', 'Clelia', 'Alessio']
+    }
+  }
+);
